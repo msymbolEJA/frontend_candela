69ae09585e00483b6795f0b2085c947f947a23f0
ed ben <edwardbenedict92@gmail.com>
2021-04-08 18:42:57 +0300

    feat: add click to navbar for redirect dasboard and button style on dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  btn: {
+    padding: 10,
+    width: 100,
+    fontSize: "1rem",
+    margin: 10,
+    cursor: "pointer",
+  },
+}));
 
 const Dashboard = () => {
   const history = useHistory();
+  const classes = useStyles();
 
   const handleClick = (event) => {
     history.push(event.target.id);
@@ -10,11 +22,11 @@ const Dashboard = () => {
   return (
     <div>
       <h1>DashBoard</h1>
-      <button id="orders/bb" onClick={handleClick}>
-        BB Orders
+      <button id="orders/bb" onClick={handleClick} className={classes.btn}>
+        Best Buy
       </button>
-      <button id="orders/ne" onClick={handleClick}>
-        NE Orders
+      <button id="orders/ne" onClick={handleClick} className={classes.btn}>
+        New Egg
       </button>
     </div>
   );
