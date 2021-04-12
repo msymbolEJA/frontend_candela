f2102afef2f5764771b83363dbffbbba61b007c7
ed ben <edwardbenedict92@gmail.com>
2021-04-12 11:20:26 +0300

    feat: add tablepagination to BBOrdersTable

--- src/components/ordertable/BestBuy/BBOrderTable.js
+++ src/components/ordertable/BestBuy/BBOrderTable.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -9,94 +8,13 @@ import TableFooter from "@material-ui/core/TableFooter";
 import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import IconButton from "@material-ui/core/IconButton";
-import FirstPageIcon from "@material-ui/icons/FirstPage";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
-import LastPageIcon from "@material-ui/icons/LastPage";
 import TableHead from "@material-ui/core/TableHead";
 import { getData } from "../../../helpers/DataTransitions";
+import TablePaginationActions from "./TablePagination";
 import spinner from "../../../assets/spinner.gif";
 import moment from "moment";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-
-const useStyles1 = makeStyles((theme) => ({
-  root: {
-    flexShrink: 0,
-    marginLeft: theme.spacing(2.5),
-  },
-}));
-
-function TablePaginationActions(props) {
-  const classes = useStyles1();
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onChangePage } = props;
-
-  const handleFirstPageButtonClick = (event) => {
-    onChangePage(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onChangePage(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onChangePage(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </div>
-  );
-}
-
-TablePaginationActions.propTypes = {
-  count: PropTypes.number.isRequired,
-  onChangePage: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
 
 const useStyles2 = makeStyles({
   table: {
@@ -126,8 +44,10 @@ export default function CustomPaginationActionsTable() {
   const [tableData, setTableData] = useState({ rows: [], count: 0 });
 
   useEffect(() => {
-    getData(`${BASE_URL}bb/?limit=100&offset=0`).then((response) => {
-      console.log(response);
+    getData(
+      `${BASE_URL}bb/?limit=${rowsPerPage}&offset=${page * rowsPerPage}`
+    ).then((response) => {
+      console.log(response.data.results);
       setTableData({
         ...tableData,
         rows: response.data.results,
@@ -135,7 +55,7 @@ export default function CustomPaginationActionsTable() {
       });
     });
     // eslint-disable-next-line
-  }, []);
+  }, [rowsPerPage, page]);
 
   const emptyRows =
     rowsPerPage -
@@ -197,13 +117,7 @@ export default function CustomPaginationActionsTable() {
           </TableHead>
           <TableBody>
             {tableData?.rows.length > 0 ? (
-              (rowsPerPage > 0
-                ? tableData?.rows?.slice(
-                    page * rowsPerPage,
-                    page * rowsPerPage + rowsPerPage
-                  )
-                : tableData?.rows
-              ).map((row, index) => (
+              tableData?.rows.map((row, index) => (
                 <TableRow key={index} className={classes.rowStyle}>
                   <TableCell
                     align="center"
@@ -230,9 +144,13 @@ export default function CustomPaginationActionsTable() {
                   <TableCell align="center">
                     <>
                       <p className={classes.priceStyle}>{row?.upc}</p>
-                      <a href={row?.url} target="_blank" rel="noreferrer">
-                        Visit
-                      </a>
+                      {row?.url ? (
+                        <a href={row?.url} target="_blank" rel="noreferrer">
+                          Visit
+                        </a>
+                      ) : (
+                        <p>No Link</p>
+                      )}
                     </>
                   </TableCell>
                   <TableCell align="center">
@@ -263,18 +181,18 @@ export default function CustomPaginationActionsTable() {
               </tr>
             )}
 
-            {emptyRows > 0 && (
+            {/* {emptyRows > 0 && (
               <TableRow style={{ height: 53 * emptyRows }}>
                 <TableCell colSpan={6} />
               </TableRow>
-            )}
+            )} */}
           </TableBody>
           <TableFooter>
             <TableRow>
               <TablePagination
-                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-                colSpan={3}
-                count={tableData?.count || 0}
+                rowsPerPageOptions={[25, 50, 100, 250, 500, 2500]}
+                colSpan={22}
+                count={tableData?.count}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 SelectProps={{
