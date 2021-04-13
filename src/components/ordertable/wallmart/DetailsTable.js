bf536bfc4e2bbc011e2ad6087a7ce6769c7a0932
ed ben <edwardbenedict92@gmail.com>
2021-04-13 11:24:08 +0300

    feat: remove console.log

--- src/components/ordertable/wallmart/DetailsTable.js
+++ src/components/ordertable/wallmart/DetailsTable.js
@@ -38,7 +38,7 @@ function Row(props) {
   const classes = useRowStyles();
 
   const detailsRow = row.items;
-  console.log(detailsRow);
+  // console.log(detailsRow);
 
   return (
     <React.Fragment>
