fed14d0b9aa24f980b7c701bcd89027690a55bb3
ed ben <edwardbenedict92@gmail.com>
2021-04-29 21:37:26 +0300

    feat: add useFetch to WAL items

--- src/components/ordertable/wallmart/Items.js
+++ src/components/ordertable/wallmart/Items.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
-import { getData } from "../../../helpers/DataTransitions";
 import { makeStyles } from "@material-ui/core/styles";
 import checkSvg from "../../../assets/check.svg";
 import warnSvg from "../../../assets/warn.svg";
+import useFetch from "../../../hooks/useFetch";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
@@ -15,31 +14,29 @@ const useStyles = makeStyles({
 });
 
 const Items = ({ dRow }) => {
-  const [upcInfos, setUpcInfos] = useState();
   const classes = useStyles();
+  const { response } = useFetch(
+    `${BASE_URL}bb/${
+      dRow?.sku.includes("NC_UPC_")
+        ? dRow.sku.replace("NC_UPC_", "")
+        : dRow?.sku.includes("MC_UPC_")
+        ? dRow.sku.replace("MC_UPC_", "")
+        : ""
+    }`
+  );
 
-  let upcQuery;
-  if (dRow?.sku.includes("NC_UPC_")) {
-    upcQuery = dRow.sku.replace("NC_UPC_", "");
-  } else if (dRow?.sku.includes("MC_UPC_")) {
-    upcQuery = dRow.sku.replace("MC_UPC_", "");
-  }
-
-  useEffect(() => {
-    getData(`${BASE_URL}bb/${upcQuery}`)
-      .then((response) => {
-        console.log("items", response.data);
-        setUpcInfos(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [upcQuery]);
+  const getUpc = () => {
+    if (dRow?.sku.includes("NC_UPC_")) {
+      return dRow.sku.replace("NC_UPC_", "");
+    } else if (dRow?.sku.includes("MC_UPC_")) {
+      return dRow.sku.replace("MC_UPC_", "");
+    }
+  };
 
   let isBuyable =
     Number(dRow?.itemPrice) / 1.3 >
-      Number(upcInfos?.salePrice) + Number(upcInfos?.shippingCost) &&
-    upcInfos?.onlineAvailability;
+      Number(response?.salePrice) + Number(response?.shippingCost) &&
+    response?.onlineAvailability;
   // console.log({ isBuyable });
 
   return (
@@ -47,7 +44,7 @@ const Items = ({ dRow }) => {
       <TableCell align="center" component="th" scope="row">
         {dRow.productName}
       </TableCell>
-      <TableCell align="center">{upcQuery}</TableCell>
+      <TableCell align="center">{getUpc()}</TableCell>
       <TableCell align="center">
         <>
           <p className={classes.priceStyle}>{dRow.itemPrice}</p>
@@ -56,23 +53,23 @@ const Items = ({ dRow }) => {
       <TableCell align="center">
         <>
           <p className={classes.priceStyle}>
-            {upcInfos?.salePrice ? upcInfos?.salePrice : "-"}
+            {response?.salePrice ? response?.salePrice : "-"}
           </p>
-          {upcInfos?.url ? (
-            <a href={upcInfos?.url} target="_blank" rel="noreferrer">
+          {response?.url ? (
+            <a href={response?.url} target="_blank" rel="noreferrer">
               Visit
             </a>
           ) : null}
         </>
       </TableCell>
-      <TableCell align="center">{upcInfos?.shippingCost}</TableCell>
+      <TableCell align="center">{response?.shippingCost}</TableCell>
       <TableCell align="center">{dRow.orderStatus}</TableCell>
       <TableCell align="center">{dRow?.taxAmount}</TableCell>
       <TableCell align="center" style={{ maxWidth: 500 }}>
         {dRow.taxName}
       </TableCell>
       <TableCell align="center">
-        {upcInfos?.onlineAvailability ? "Yes" : "No"}
+        {response?.onlineAvailability ? "Yes" : "No"}
       </TableCell>
       <TableCell align="center">
         <img
